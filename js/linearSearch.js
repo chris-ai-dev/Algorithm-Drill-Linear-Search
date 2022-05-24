c8780d6815d18b13f6c2703b159303b7128d9023
Chris <chrisvolar@gmail.com>
2022-05-24 16:49:57 -0500

    linear search

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -1,9 +1,25 @@
-const arrayToSearchThrough = [];
-for (let i = 1; i <= 1000; i++) {
-    arrayToSearchThrough.push(i);
+const linear =(name)=>{
+    
+    const arrayToSearchThrough = [];
+    for (let i = 0; i < name.length; i++) {
+    arrayToSearchThrough.push(name[i]);
+    }
+    return arrayToSearchThrough;
 }
 
-exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
+const randomList = linear("banana")
 
-};
+const linearSearch =(item,array)=>{
+    const indexsArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if( array[i] === item) {
+        indexsArray.push(i);
+        }
+    }
+    return indexsArray;
+}
+
+console.log(linearSearch('a',randomList))
+// exports.linearSearch = function(valueToFind, arrayToSearchThrough) ({}
+// };
 
